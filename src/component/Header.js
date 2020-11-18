7da3a2a714b995350b165f9b15800e57b3db14a6
--replace-all <snehakandpal157@gmail.com>
2020-11-18 23:18:19 +0530

    header image added

---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header-wrap">
+      <img className="header-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx0RVb8nIhm7gMeQ25Di6-eAuCJ7gf8QTJtA&usqp=CAU" alt="header-img"/>
+    </div>
+  )
+}
