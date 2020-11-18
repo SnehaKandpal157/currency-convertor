af75e28523af19c92c03c8475bd8c6ec05fb0fef
--replace-all <snehakandpal157@gmail.com>
2020-11-18 23:14:59 +0530

    first commit

---
+++ src/component/Form.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { Label, Input } from 'reactstrap';
+import { currencyOptions } from "../constants";
+import { Select, MenuItem, Button } from "@material-ui/core";
+import _isEmpty from "lodash/isEmpty";
+import axios from 'axios';
+
+const Form = () => {
+  const [inputAmount, setInputAmount] = useState("");
+  const [outputAmount, setOutputAmount] = useState("");
+  const [inputCurrency, setInputCurrency] = useState("");
+  const [outputCurrency, setOutputCurrency] = useState("");
+
+
+  const handleInputCurrency = (e) => {
+    const { value } = e.target;
+    setInputCurrency(value)
+  }
+
+  const handleOutputCurrency = (e) => {
+    const { value } = e.target;
+    setOutputCurrency(value)
+  }
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setInputAmount(value);
+  };
+
+  const handleConvert = () => {
+    axios.get(`https://api.frankfurter.app/latest?amount=${inputAmount}&from=${inputCurrency}&to=${outputCurrency}`)
+      .then(res => {
+        const data = res.data.rates;
+        setOutputAmount(data[outputCurrency])
+      })
+  }
+
+  return (
+    <div className="outer-wrap">
+      <div className="input-wrap">
+        <Label className="label">Input Amount</Label>
+        <Input
+          type="number"
+          onChange={handleInputChange}
+          name="inputAmount"
+          value={inputAmount}
+          placeholder="Input Amount"
+          className="input"
+        />
+        <Select
+          name="inputCurrency"
+          value={inputCurrency}
+          onChange={handleInputCurrency}
+        >
+          {currencyOptions.map((option, index) => (
+            <MenuItem value={option.value} key={index}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
+      <div className="button-wrap">
+        <Button onClick={handleConvert} variant="contained" color="primary">Convert</Button>
+      </div>
+      <div className="output-wrap">
+        <Label className="label">Output Amount</Label>
+        <Input
+          readOnly
+          name="outputAmount"
+          value={outputAmount}
+          placeholder="Output Amount"
+          className="input"
+        />
+
+        <Select
+          name="outputCurrency"
+          value={outputCurrency}
+          onChange={handleOutputCurrency}
+        >
+          {currencyOptions.map((option, index) => (
+            <MenuItem value={option.value} key={index}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
+    </div>
+  )
+}
+
+export default Form
