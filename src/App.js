fc4e1bbc26d4bad920dbaafb1cf5968f4671316b
--replace-all <snehakandpal157@gmail.com>
2020-11-18 23:16:23 +0530

    created header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./component/Header"
 import Form from "./component/Form";
 
 function App() {
   return (
     <div className="main-wrapper">
+      <Header />
       <Form />
     </div>
   );
